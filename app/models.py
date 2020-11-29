1912d7ac3e84f86b132cb5685ac090ec3f191a4f
ashwani <ashwani.rtm@gmail.com>
2020-11-29 23:57:35 +0530

    added more functionality to the bare bones app

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
+from app import login
 from app import db
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(255), index=True, unique=True)
@@ -19,6 +21,11 @@ class User(db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash,password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(255))
